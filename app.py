5d1e80d31a88dd8d35ab575e76350bc77d3fd5ec
abhiav-spec <kumarabhinav6649@gmail.com>
2025-04-11 01:27:44 +0530

    model updated

---
+++ app.py
@@ -0,0 +1,21 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+flask_app = Flask(__name__)
+model = pickle.load(open('crop_model.pkl', 'rb'))
+
+
+@flask_app.route('/')
+def Home():
+    return render_template('index.html')
+
+@flask_app.route('/predict',methods=['POST'])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(float_features)]
+    prediction=model.predict(final_features)
+    return render_template('index.html', prediction=f"The predicted crop is {prediction[0]}")
+
+if __name__ == "__main__":
+    flask_app.run(debug=True)   
